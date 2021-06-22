c936da0cf9408549223690e5c373adb84573eb99
slippedandmissed <gbt505@hotmail.com>
2021-06-22 13:54:33 +0100

    Hid "Log in to get started" message when logged in

--- frontend/src/app/pages/homepage/homepage.component.ts
+++ frontend/src/app/pages/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserDataService } from 'src/app/services/user-data.service';
 
 @Component({
   selector: 'app-homepage',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public user: UserDataService) { }
 
   ngOnInit(): void {
   }
